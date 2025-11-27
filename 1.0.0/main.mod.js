3b1f8369173bee85c11f0399303a64f2682a12d1
DoraChad <176503791+DoraChad@users.noreply.github.com>
2025-11-26 22:10:24 -0800

    this aint gonna work but hey, you gotta try it

--- 1.0.0/main.mod.js
+++ 1.0.0/main.mod.js
@@ -2,14 +2,15 @@ import { PolyMod, MixinType } from "https://pml.crjakob.com/cb/PolyTrackMods/Pol
 
 class RPC extends PolyMod {
     init = (pml) => {
-        // regular init
-    }
-    postInit = () => {
-        // post init
-    }
-    simInit = () => {
-        // sim init here
-    } 
+        pml.registerFuncMixin("polyInitFunction", MixinType.INSERT, 'const n = i(7780);', `
+            const script = document.createElement("script");
+            script.src = "https://raw.githubusercontent.com/discordjs/RPC/refs/heads/master/src/client.js"; // URL of the library
+            script.onload = () => {
+                console.log("loaded!");
+            };
+            document.head.appendChild(script);
+    `);
+    }} 
 }
 
 
