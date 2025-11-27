ce18488fbc77d41534f46d21c64549ed8ae0286e
DoraChad <176503791+DoraChad@users.noreply.github.com>
2025-11-26 22:16:18 -0800

    Update main.mod.js

--- 1.0.0/main.mod.js
+++ 1.0.0/main.mod.js
@@ -4,7 +4,7 @@ class RPC extends PolyMod {
     init = (pml) => {
         pml.registerFuncMixin("polyInitFunction", MixinType.INSERT, 'const n = i(7780);', `
             const script = document.createElement("script");
-            script.src = "https://raw.githubusercontent.com/discordjs/RPC/refs/heads/master/src/client.js"; // URL of the library
+            script.src = "https://raw.githubusercontent.com/discordjs/RPC/refs/heads/master/src/client.js";
             script.onload = () => {
                 console.log("loaded!");
             };
